fffe82deeaad7d2e5bec824c940582f140435b48
Dmitry Ivanov <me@d-ops.pro>
2021-04-22 09:49:48 +0300

    Add readiness check endpoint

--- monit/http_metrics.go
+++ monit/http_metrics.go
@@ -10,6 +10,7 @@ import (
 
 func ConfigureRouter(r *mux.Router) {
 	r.Methods(http.MethodGet).Path("/health").HandlerFunc(healthCheckHandler)
+	r.Methods(http.MethodGet).Path("/ready").HandlerFunc(healthCheckHandler)
 }
 
 func healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
@@ -20,5 +21,16 @@ func healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
 	err := json.NewEncoder(w).Encode(result)
 	if err != nil {
 		logrus.WithError(err).Errorf("unable to write health response")
+	}
+}
+
+func readyCheckHandler(w http.ResponseWriter, _ *http.Request) {
+	result := map[string]interface{}{
+		"status": "OK",
+	}
+
+	err := json.NewEncoder(w).Encode(result)
+	if err != nil {
+		logrus.WithError(err).Errorf("unable to write ready response")
 	}
 }
